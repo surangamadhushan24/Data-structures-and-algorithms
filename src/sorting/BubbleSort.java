56c74f385ccee4ecefb98a71df9fcf7052c708a1
suranga madhushan <150942338+surangamadhushan24@users.noreply.github.com>
2026-08-29 12:40:11 +0530

    feat:implement bubble sort:

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -1,14 +1,38 @@
 package sorting;
 
+import java.util.Arrays;
+
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {8,5,6,4,3,5,2};
+        // 1 passes {5,6,4,3,5,2,8}
+        // 2 passes {5,4,3,5,2,6,8}
+
         bubbleSort(arr);
     }
 
     private static void bubbleSort(int[] arr) {
-        for(int i=0;i<arr.length-1;i++){
-            for (int j=0;)
+
+        boolean isSorted;
+
+        for(int j = 0; j <arr.length-1; j++){
+            isSorted = true;
+            for(int i=0;i<arr.length-1 -j;i++){
+                
+                if(arr[i]>arr[i+1]){
+                    
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                    isSorted = false;
+                }
+
+            }
+            if(isSorted){
+                return;
+            }
         }
+
+        System.out.println(Arrays.toString(arr));
     }
 }

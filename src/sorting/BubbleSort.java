d4b953a808948da32f41aaba0b3ec88efe177da8
suranga madhushan <150942338+surangamadhushan24@users.noreply.github.com>
2026-09-01 23:02:50 +0530

    feat:selection sort

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -4,35 +4,30 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {8,5,6,4,3,5,2};
-        // 1 passes {5,6,4,3,5,2,8}
-        // 2 passes {5,4,3,5,2,6,8}
+        int[] arr = {8, 5, 6, 4, 3, 5, 2};
         bubbleSort(arr);
-    }
-
-    private static void bubbleSort(int[] arr) {
-
-        boolean isSorted;
-
-        for(int j = 0; j <arr.length-1; j++){
-            isSorted = true;
-            for(int i=0;i<arr.length-1;i++){
-                
-                if(arr[i]>arr[i+1]){
-                    swap(arr,arr[i],arr[i+1] );
-                    isSorted = false;
-                }
-
-            }
-            if(isSorted){
-                return;
-            }
-        }
-
         System.out.println(Arrays.toString(arr));
     }
 
-    private static void swap(int[] arr, int index1, int index2){
+    private static void bubbleSort(int[] arr) {
+        boolean isSorted;
+
+        for (int j = 0; j < arr.length - 1; j++) {
+            isSorted = true;
+            for (int i = 0; i < arr.length - 1 - j; i++) {
+                if (arr[i] > arr[i + 1]) {
+
+                    swap(arr, i, i + 1);
+                    isSorted = false;
+                }
+            }
+            if (isSorted) {
+                return;
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int index1, int index2) {
         int temp = arr[index1];
         arr[index1] = arr[index2];
         arr[index2] = temp;
--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -1,2 +1,31 @@
-package sorting;public class SelectionSort {
+package sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {7, 2, 8, 3, 6, 8, 4, 5};
+        selectionSort(arr);
+    }
+
+    private static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                swap(arr, minIndex, i);
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
 }

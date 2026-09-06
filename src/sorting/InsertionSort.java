3a76a739a7ee235397f2f432d2103a3e84fa4d1f
suranga madhushan <150942338+surangamadhushan24@users.noreply.github.com>
2026-09-06 15:46:13 +0530

    feat:implement insertion sort algorithm

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -1,2 +1,23 @@
-package sorting;public class InsertionSort {
+package sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {12,11,13,5,6};
+        insertionSort(arr);
+    }
+
+    private static void insertionSort(int[] arr) {
+        for(int i =1;i< arr.length;i++){
+            int current = arr[i];
+            var j = i-1;
+            while(j>=0 && arr[j]> current){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = current;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }

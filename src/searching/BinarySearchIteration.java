7e703d0c01d5dc1fc503fe7293204215ab5c04fb
suranga madhushan <150942338+surangamadhushan24@users.noreply.github.com>
2026-09-13 14:02:51 +0530

    feat:implement Binary search algorithm using iteration

---
+++ src/searching/BinarySearchIteration.java
@@ -0,0 +1,30 @@
+package searching;
+
+public class BinarySearchIteration {
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,8,8,9,12,13};
+
+        System.out.println(binarySearchIteration(arr,8));
+    }
+
+    private static int binarySearchIteration(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length-1;
+        while (left<=right){
+            int middle = (left+right)/2;
+            if(arr[middle] == target)
+                return middle;
+
+            if(arr[middle]>target){
+                right = middle -1;
+            }
+            else {
+                left = middle +1;
+            }
+
+        }
+        return -1;
+    }
+
+
+}
